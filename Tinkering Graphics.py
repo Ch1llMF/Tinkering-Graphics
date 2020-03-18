7f31c6850c9867ffd3a747f5e3133ef47bdbdfce
Ch1llMF <ji229970@falmouth.ac.uk>
2020-03-18 04:49:05 +0000

    Added code of all the colours that I will allow the user to switch between.

--- Tinkering Graphics.py
+++ Tinkering Graphics.py
@@ -1,6 +1,12 @@
 import pygame as pg
 pg.init()
 
+Red = (237, 28, 36)
+Green = (34, 177, 76)
+Blue = (63, 72, 204)
+Yellow = (255, 242, 0)
+Orange = (255, 127, 39)
+
 def colourSwap(surf, from_, to_):
     arr = pg.PixelArray(surf)
     arr.replace(Red, Blue)
