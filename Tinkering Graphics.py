c25f28233ba43966fce47136375ab1007bccbfb1
Ch1llMF <ji229970@falmouth.ac.uk>
2020-03-18 02:50:35 +0000

    Added code to open a window using pygame.

---
+++ Tinkering Graphics.py
@@ -0,0 +1,12 @@
+import pygame as pg
+pg.init()
+
+
+screen = pg.display.set_mode((800, 600))
+done = False
+while not done:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            done = True
+        player.draw(screen)
+        pg.display.update()
